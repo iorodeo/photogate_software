1bb22170507a8b15272ed226a01f008343e49009
Will Dickson <will@iorodeo.com>
2014-08-13 22:35:18 -0700

    Worked on photogate GUI.

--- gui/python/photogate/photogate/photogate_app.py
+++ gui/python/photogate/photogate/photogate_app.py
@@ -1,8 +1,10 @@
 from __future__ import print_function
 import sys
+import serial.tools.list_ports
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 from photogate_ui import Ui_PhotogateMainWindow
+from photogate_serial import PhotogateDevice
 
 
 class PhotogateMainWindow(QtGui.QMainWindow, Ui_PhotogateMainWindow):
@@ -16,6 +18,7 @@ class PhotogateMainWindow(QtGui.QMainWindow, Ui_PhotogateMainWindow):
     def connectWidgets(self):
         self.connectPushButton.pressed.connect(self.connectPressed_Callback)
         self.connectPushButton.clicked.connect(self.connectClicked_Callback)
+        self.scanPushButton.clicked.connect(self.scanClicked_Callback)
         self.resetPushButton.clicked.connect(self.resetClicked_Callback)
         self.actionSaveMat.triggered.connect(self.saveMat_Callback)
         self.actionSaveTxt.triggered.connect(self.saveTxt_Callback)
@@ -26,12 +29,62 @@ class PhotogateMainWindow(QtGui.QMainWindow, Ui_PhotogateMainWindow):
 
     def initialize(self):
         self.dev = None
+        self.populatePortComboBox()
+        self.updateWidgetEnabled()
+        self.statusbar.showMessage('Not Connected')
 
     def connectPressed_Callback(self):
-        print('connect pressed')
+        if self.dev is None:
+            self.connectPushButton.setText('Disconnect')
+            self.connectPushButton.setFlat(True)
+            self.portComboBox.setEnabled(False)
+            self.statusbar.showMessage('Connecting...')
 
     def connectClicked_Callback(self):
-        print('connect clicked')
+        if self.dev is None:
+            self.connectDevice()
+        else:
+            self.disconnectDevice()
+        self.updateWidgetEnabled()
+        self.connectPushButton.setFlat(False)
+
+    def connectDevice(self):
+        port = str(self.portComboBox.currentText())
+        try:
+            self.dev = PhotogateDevice(port)
+        except Exception, e: 
+            msgTitle = 'Connection Error'
+            msgText = 'unable to connect to device: {0}'.format(str(e))
+            QtGui.QMessageBox.warning(self,msgTitle, msgText)
+            self.connectPushButton.setText('Connect')
+            self.statusbar.showMessage('Not Connected')
+            self.dev = None
+        if self.dev is not None:
+            self.statusbar.showMessage('Connected')
+
+    def disconnectDevice(self):
+        if self.dev is not None:
+            try:
+                self.dev.close()
+            except:
+                pass
+            self.dev = None
+        self.connectPushButton.setText('Connect')
+        self.statusbar.showMessage('Not Connected')
+
+    def updateWidgetEnabled(self):
+        print('update widget enabled')
+
+    def populatePortComboBox(self):
+        self.portComboBox.clear()
+        portList = serial.tools.list_ports.comports()
+        for portName, portDesc, portId in portList:
+            if 'USB' in portName or 'ACM' in portName:
+                self.portComboBox.addItem(portName)
+        self.portComboBox.setCurrentIndex(0)
+
+    def scanClicked_Callback(self):
+        self.populatePortComboBox()
 
     def resetClicked_Callback(self):
         print('reset clicked')
--- gui/python/photogate/photogate/photogate_serial.py
+++ gui/python/photogate/photogate/photogate_serial.py
@@ -4,14 +4,13 @@ import platform
 import time
 import json
 
-
-class PhotogateDev(serial.Serial):
+class PhotogateDevice(serial.Serial):
 
     BAUDRATE = 115200
     RESET_TIMEOUT = 2.0
 
     def __init__(self, port='/dev/ttyACM0', timeout=1.0):
-        super(PhotogateDev,self).__init__(port,self.BAUDRATE,timeout=timeout)
+        super(PhotogateDevice,self).__init__(port,self.BAUDRATE,timeout=timeout)
         time.sleep(self.RESET_TIMEOUT)
         self.reset()
         self.flushInput()
@@ -31,7 +30,7 @@ class PhotogateDev(serial.Serial):
 if __name__ == '__main__': 
 
     port = '/dev/ttyACM0'
-    dev = PhotogateDev(port)
+    dev = PhotogateDevice(port)
 
     while True:
         dataDict = dev.getData()
